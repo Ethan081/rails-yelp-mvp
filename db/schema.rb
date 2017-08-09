682270ad590f860793c669de023b006a637efb1a
etienne ziegelmeyer <ziegelmeyer.etienne@gmail.com>
2017-08-09 11:50:08 +0200

    Add review model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170809093830) do
+ActiveRecord::Schema.define(version: 20170809094650) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 20170809093830) do
     t.string   "categorie"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "rating"
+    t.string   "reference"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
